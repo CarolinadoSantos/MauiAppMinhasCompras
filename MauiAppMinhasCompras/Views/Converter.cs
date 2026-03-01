dcfeb64b999c48da9c06faedd634f767526ca60c
Carol Santos <carol.natalia@gmail.com>
2026-03-01 15:22:43 -0300

    Lista de produto

---
+++ MauiAppMinhasCompras/Views/Converter.cs
@@ -0,0 +1,11 @@
+﻿
+namespace MauiAppMinhasCompras.Views
+{
+    internal class Converter
+    {
+        internal static double ToDouble(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
@@ -1,3 +1,7 @@
+using MauiAppMinhasCompras.Models;
+using System;
+using System.Threading.Tasks;
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class NovoProduto : ContentPage
@@ -6,4 +10,29 @@ public partial class NovoProduto : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async Task ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			Produto p = new Produto 
+			{
+				Descricao = txt_descricao.Text,
+				Quantidade = Converter.ToDouble (txt_quantidade.Text),
+				Preco = Converter.ToDouble(txt_preco.Text),
+            };
+
+			await App.Db.Insert(p);
+			await DisplayAlert("Sucesso!", "Registro Inserido", "OK");
+
+		}catch (Exception ex)
+		{
+			DisplayAlert("Ops", ex.Message, "OK");
+		}
+    }
+
+    private void ToolbarItem_Clicked_1(object sender, EventArgs e)
+    {
+
+    }
 }
